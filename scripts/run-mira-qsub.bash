85b79184fefc82a0412815723a05e43494661bea
Wu, Ke <wuke@cs.umd.edu>
2014-03-27 02:42:01 -0400

    Imporve run-mira-qsub.bash
    1. Retry an iteration for up to MAXTRY times in case of failure.
    2. INIT_WEIGHTS is no longer required when ITERS is set to continue an optimization run.
    3. Compress logs from qsub.
    4. Add MEM option.

--- scripts/run-mira-qsub.bash
+++ scripts/run-mira-qsub.bash
@@ -2,7 +2,8 @@
 
 function usage {
     cat <<EOF
-Usage: INPUT=x OUTPUT=y INIT_WEIGHTS=z [ITERS=w METRIC=v] $0 ... decoder command ...
+Usage: INPUT=x OUTPUT=y [INIT_WEIGHTS=z ITERS=50 METRIC=ibm_bleu MEM=4g MAXTRY=5] $0 ... decoder command ...
+Use ITERS="x y" to restart from previous runs, otherwise INIT_WEIGHTS is required.
 EOF
 }
 
@@ -13,26 +14,25 @@ function log {
 
 set -e
 
-QSUB_OPTS=(-q wide -l pmem=7g,walltime=2:00:00)
-
 SUBMIT=`which qsub-mr.bash`
 MAPPER=`which kbest_mirav5`
 REDUCER=`which mr_mira_reduce`
 
 [ "x$INPUT" != x ] || { log "Set INPUT"; exit 1; }
 [ "x$OUTPUT" != x ] || { log "Set OUTPUT"; exit 1; }
-[ "x$INIT_WEIGHTS" != x ] || { log "Set INIT_WEIGHTS"; exit 1; }
+[ "x$MEM" != x ] || MEM=4g
+[ "x$MAXTRY" != x ] || MAXTRY=5
+[ "x$ITERS" != x ] || ITERS=50
+[ "x$METRIC" != x ] || METRIC=ibm_bleu
+if ! echo "$ITERS" | grep -q ' '; then
+    [ "x$INIT_WEIGHTS" != x ] || { log "Set INIT_WEIGHTS"; exit 1; }
+fi
+
 log "INPUT = $INPUT"
 log "OUTPUT = $OUTPUT"
 log "INIT_WEIGHTS = $INIT_WEIGHTS"
 
-if [ "x$ITERS" = x ]; then
-    ITERS=50
-fi
-
-if [ "x$METRIC" = x ]; then
-    METRIC=ibm_bleu
-fi
+QSUB_OPTS=(-q wide -l pmem="$MEM",walltime=2:00:00)
 
 if ! echo "$ITERS" | grep -q ' '; then
     [ ! -e "$OUTPUT" ] || { log "$OUTPUT already exists"; exit 1; }
@@ -46,15 +46,28 @@ for i in `seq $ITERS`; do
     LAST_ITER=$(($i-1))
     LAST_WEIGHTS="$OUTPUT/`printf %04d $LAST_ITER`/weights.out"
     LAST_WEIGHTS=`readlink -f "$LAST_WEIGHTS"`
-    /usr/bin/time -f%e "$SUBMIT" \
-	-m "$MAPPER -w $LAST_WEIGHTS -m $METRIC -s 1000 -a -b 1000 -o 2 -p 0 -- $*" \
-	-r "$REDUCER" \
-	-i "$INPUT" \
-	-o "$WORKDIR" \
-	-w -c -- "${QSUB_OPTS[@]}"
+    if [ ! -e "$LAST_WEIGHTS" ]; then
+	log "cannot find $LAST_WEIGHTS"
+	exit 1
+    fi
+    for j in $(seq "$MAXTRY"); do
+	if "$SUBMIT" \
+	    -m "$MAPPER -w $LAST_WEIGHTS -m $METRIC -s 1000 -a -b 1000 -o 2 -p 0 -- $*" \
+	    -r "$REDUCER" \
+	    -i "$INPUT" \
+	    -o "$WORKDIR" \
+	    -w -c -- "${QSUB_OPTS[@]}"; then
+	    break
+	fi
+    done
+    if [ ! -e "$WORKDIR/reduce.success" ]; then
+	log "all $MAXTRY runs failed"
+	exit 1
+    fi
 
     n=`zcat "$WORKDIR/reduce.out.gz" | wc -l`
     n=$(($n-1))
-    zcat "$WORKDIR/reduce.out.gz" | head -n$n | sort -n | cut -f2 | gzip - > "$WORKDIR/best.out.gz"
+    gzip "$WORKDIR"/*.err &
+    zcat "$WORKDIR/reduce.out.gz" | head -n$n | sort -n | cut -f2 | gzip - > "$WORKDIR/best.out.gz" &
     zcat "$WORKDIR/reduce.out.gz" | tail -n1 | sed -e 's/\t/\n/g' | LC_ALL=C sort > "$WORKDIR/weights.out"
 done
